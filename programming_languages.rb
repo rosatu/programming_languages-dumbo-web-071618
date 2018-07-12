a4c0d40f4cd0e7a74349296c0fa2541657499585
Rosa Tu <rosalina.tu@gmail.com>
2018-07-12 21:40:15 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,5 +2,9 @@ require 'pry'
 def reformat_languages(languages)
   hash = {}
   languages.each do |language_type, language_name_hash|
-  binding.pry  
+  hash << language_name_hash 
+  language_name_hash.each do |language_name, language_hash|
+  binding.pry
+  end  
+  end
 end  
