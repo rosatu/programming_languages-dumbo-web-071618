b48aae87df1202144bdac4b19fbafc1acff5fcbe
Rosa Tu <rosalina.tu@gmail.com>
2018-07-12 22:30:53 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,5 +1,6 @@
 def reformat_languages(languages)
   new_hash = languages[:oo].merge(languages[:functional])
+  binding.pry
   new_hash.each do |k,v|
     if languages[:oo].keys.include?(k)
       hash["#{k}"][:style] = :oo
