d0f03e0e3927adf9b644e487031c63e26840dadd
Rosa Tu <rosalina.tu@gmail.com>
2018-07-12 22:17:41 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,9 +1,11 @@
 require 'pry'
 def reformat_languages(languages)
   hash = languages[:oo].merge(languages[:functional])
-  hash.each do |name, type_hash|
-    if language[:oo].keys.include?(name)
-      hash[name] = {:style => [:oo]}
   binding.pry
+  hash.each do |name, type_hash|
+    if languages[:oo].keys.include?(name)
+      hash[name] = {:style => [:oo]}
+    end
+  end 
   return hash
 end  
