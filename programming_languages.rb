bf47c1971cb3cc19256c29621b1094914ba71545
Rosa Tu <rosalina.tu@gmail.com>
2018-07-12 22:41:36 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,9 +2,9 @@ require 'pry'
 def reformat_languages(languages)
   new_hash = languages[:oo].merge(languages[:functional])
   new_hash.each do |k,v|
-    if languages[:oo].keys.include?(k) && 
+    if languages[:oo].keys.include?(k) && languages[:functional].keys.exclude?(k)
       new_hash[k][:style] = [:oo]
-    elsif languages[:functional].keys.include?(k)
+    elsif languages[:functional].keys.include?(k) && languages[:oo].keys.exclude?(k)
       new_hash[k][:style] = [:functional]
     elsif languages[:functional].keys.include?(k) && languages[:oo].keys.include?(k)
       new_hash[k][:style] = [:oo, :functional]
